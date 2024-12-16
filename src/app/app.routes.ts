12bc1faae610e0aad7ddb80419c031b926d51f37
Hajder Al-Remahy <hajduty@outlook.com>
2024-12-16 20:48:30 +0000

    add navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { BookComponent } from './book/book.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "", redirectTo: "/book", pathMatch: "full"},
+    { path: 'book', component: BookComponent }
+];
