94b9de2064425006b4571b01477f417ad8b01661
Hajder Al-Remahy <hajduty@outlook.com>
2024-12-16 21:35:55 +0000

    change file structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { BookComponent } from './book/book.component';
+import { BookComponent } from './features/book/book.component';
 
 export const routes: Routes = [
     { path: "", redirectTo: "/book", pathMatch: "full"},
