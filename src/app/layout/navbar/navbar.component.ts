ae2310d0618dd9beb6e30a87e6d0668aa87f5680
Hajder Al-Remahy <hajduty@outlook.com>
2024-12-17 03:59:12 +0000

    change navbar styling

--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -1,19 +1,35 @@
 import { Component, computed } from '@angular/core';
 import { DarkModeService } from '../../core/services/darkmode.service';
+import { AuthService } from '../../core/services/auth.service';
+import { Router, RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
   isDark = computed(() => this.darkModeService.darkMode());
 
-  constructor(private darkModeService: DarkModeService) {}
+  constructor(private darkModeService: DarkModeService, private authService: AuthService, private router: Router) { }
 
   toggleTheme() {
     this.darkModeService.toggleDarkMode();
+  }
+
+  async onLogout() {
+    try {
+      await this.authService.logout();
+      this.router.navigate(['/login']);
+    } catch (error) {
+      console.error('Logout failed', error);
+    }
+  }
+
+  isLoggedIn(): boolean {
+    return this.authService.isAuthenticated();
   }
 }
